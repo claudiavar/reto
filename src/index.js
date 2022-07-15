4227cadae4f59d6d4e2008f043c28a8e31f74154
jherry-Ponce <jherry_libra23@hotmail.com>
2022-07-14 20:39:10 -0500

    grid

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Navbar } from './components/Header/Nabvar';
+import { Navbar } from './components/Header/Grid';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
